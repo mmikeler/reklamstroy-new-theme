d9e3e9831a6d25aeaeedc80c0235323a10b81d47
mmikeler <mmikeler@mail.ru>
2021-10-08 21:29:50 +0300

    70%

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+$('body').on('click', '#faq .header', function (e) {
+  const target = e.target;
+  $(target).next().slideToggle();
+  $(target).parent().toggleClass('open');
+})
